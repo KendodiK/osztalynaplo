c0a52d66d09f3f64e067927bdb18fe9bd49235bb
3111TAN-16 <3111TAN-16@tanuzem.boronkay.local>
2025-04-07 12:47:57 +0200

    get_marks_by_teachers

--- laravel/app/Models/Teacher.php
+++ laravel/app/Models/Teacher.php
@@ -16,4 +16,28 @@ class Teacher extends EloquentModel
     {
         return $this->hasMany(Group::class,'class_id');
     }
+
+    public static function marksForSubjectByTeacher($group_id,$subject_name)
+    {
+        $groupId = $group_id;
+        $subjectName = $subject_name;
+
+        $marks = Mark::join('students', 'marks.student_id', '=', 'students.id')
+            ->join('subjects', 'marks.subject_id', '=', 'subjects.id')
+            ->select('students.name', 'students.group_id', 'subjects.subject_name', 'marks.mark', 'marks.given_at')
+            ->whereIn('marks.student_id', function ($query) {
+                $query->select('id')
+                    ->from('students')
+                    ->where('group_id', $groupId);
+            })
+            ->whereIn('marks.subject_id', function ($query) {
+                $query->select('id')
+                    ->from('subjects')
+                    ->where('subject_name', $subjectName);
+            })
+            ->orderBy('marks.given_at')
+            ->get();
+
+        return $marks;
+    }
 }
