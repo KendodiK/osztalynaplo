962c8670c6b58b16da879b13b075921c81c7f400
LCervus <102804734+KendodiK@users.noreply.github.com>
2025-04-14 10:11:32 +0200

    class tabel done (with marks)

--- laravel/public/js/app.js
+++ laravel/public/js/app.js
@@ -15,40 +15,70 @@ $(document).ready(function () {
 
                 if(len > 0){
                     var toHtml =
-                        "<ul>" +
-                        "<li>" +
-                        "<div>Név</div>" +
-                        "<div>Szept.</div>" +
-                        "<div>Okt.</div>" +
-                        "<div>Nov.</div>" +
-                        "<div>Dec.</div>" +
-                        "<div>Jan.</div>" +
-                        "<div>Febr.</div>" +
-                        "<div>Márc.</div>" +
-                        "<div>Ápr.</div>" +
-                        "<div>Máj.</div>" +
-                        "<div>Jun.</div>" +
-                        "</li>";
+                        "<table> <thead>" +
+                        "<tr class='class-table-head'>" +
+                        "<th>Név</th>" +
+                        "<th>Szept.</th>" +
+                        "<th>Okt.</th>" +
+                        "<th>Nov.</th>" +
+                        "<th>Dec.</th>" +
+                        "<th>Jan.</th>" +
+                        "<th>Febr.</th>" +
+                        "<th>Márc.</th>" +
+                        "<th>Ápr.</th>" +
+                        "<th>Máj.</th>" +
+                        "<th>Jun.</th>" +
+                        "</tr></thead> <tbody>";
 
-                    for (var i = 0; i < len; i++){
-                        toHtml += "<li><div>" + response[i].name + "</div><div id='classTableR-" + i + "' class='ClassTableR'></div></li>"
+                    var cls = 'odd';
+                    var last = ' ';
+                    var id = 0;
+                    for (var i = 0; i < len; i++) {
+                        if (response[i].name != last) {
+                            last = response[i].name;
+                            if (i % 2 === 0) {
+                                cls = 'even-tr';
+                            } else {
+                                cls = 'odd-tr';
+                            }
+                            toHtml += "<tr class='" + cls + " stud-" + i + "' id='" + id + "'>" +
+                                "<td>" + response[i].name + "</td>" +
+                                "<td class='month1' id='month1'></td>" +
+                                "<td class='month2' id='month2'></td>" +
+                                "<td class='month3' id='month3'></td>" +
+                                "<td class='month4' id='month4'></td>" +
+                                "<td class='month5' id='month5'></td>" +
+                                "<td class='month6' id='month6'></td>" +
+                                "<td class='month7' id='month7'></td>" +
+                                "<td class='month8' id='month8'></td>" +
+                                "<td class='month9' id='month9'></td>" +
+                                "<td class='month10' id='month10'></td>" +
+                                "</tr>"
+                            id++;
+                        }
                     }
-                    toHtml += "</ul>"
+                    toHtml += "</tbody> </talbe>"
                     $('#teacherContent').html(toHtml);
-                    createClassTable(response);
+
+                    for(var i = 0; i < len; i++){
+                        createClassTable(i, response[i].mark, response[i].given_at);
+                    }
                 }
             }
         })
     })
 
-    function createClassTable(data){
-        for(var i = 0; i < data.length; i++){
-            var row;
-            for(var j = 0; j < 10; j++){
-                row += "<div></div>"
+    function createClassTable(studentPlace, mark, given_at){
+        var given = given_at.substring(5,7);
+        if(given[0] == '0'){
+            given = given[1];
+        }
+        var monthClass = ['month1', 'month2', 'month3', 'month4', 'month5', 'month6', 'month7', 'month8', 'month9', 'month10']
+        var month = ['9', '10', '11', '12', '1', '2', '3', '4', '5', '6']
+        for(var i = 0; i < month.length; i++) {
+            if(month[i] == given){
+                $("#"+studentPlace+" #"+monthClass[i]).html(mark);
             }
-            var id = "#classTableR" + i
-            $(id).html(row);
         }
     }
 })
