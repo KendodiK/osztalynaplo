7b7b0ad31b0befdf01359d4098e7e7ae595212ef
LCervus <102804734+KendodiK@users.noreply.github.com>
2025-05-19 10:54:57 +0200

    add panel styled

--- laravel/resources/views/teacherPage/create.blade.php
+++ laravel/resources/views/teacherPage/create.blade.php
@@ -1,21 +1,27 @@
 @extends('layout')
 
 @section('content')
-<div>
-    <h1>Hozzáadás</h1>
-</div>
-
 <form action="{{ route('marks.add') }}" method="post">
     @csrf
     <fieldset>
-        <label for="name">Név: {{$student->name}} ({{$student->id}})</label>
-        <input type="hidden" name="student" value="{{$student->id}}">
-        <label for="subject_id">Tantárgy: {{$subject->subject_name}}</label>
-        <input type="hidden" name="subject" value="{{$subject->id}}">
-        <label for="mark">Jegy:</label>
-        <input type="number" min="1" max="5" name="mark" >
-        <label for="date">Dátum:</label>
-        <input type="date" name="date">
+        <div class="display-subject">
+            <label>Jegy hozzáadása - {{ $subject->subject_name }}</label>
+            <input type="hidden" name="subject" value="{{$subject->id}}">
+        </div>
+        <div class="add-name">
+            <label for="name">{{$student->name}} (id:{{$student->id}})</label>
+            <input type="hidden" name="student" value="{{$student->id}}">
+        </div>
+        <div class="editable">
+            <div class="set-mark-time">
+                <label for="date">Dátum:</label>
+                <input type="date" name="date">
+            </div>
+            <div class="set-mark">
+                <label for="mark">Jegy:</label>
+                <input type="number" min="1" max="5" name="mark" >
+            </div>
+        </div>
     </fieldset>
     <div class="save-buttons">
         <button type="submit" name="return">Mégse</button>
